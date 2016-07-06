674e73aaf252722da7e60a080567ec15596e472e
Sam Shapiro <samuel.shapiro123@gmail.com>
2016-07-06 19:50:03 -0400

    added output to secondHello

--- second/src/main/java/second/SecondHello.java
+++ second/src/main/java/second/SecondHello.java
@@ -6,8 +6,12 @@ public class SecondHello {
 	public static void main(String[] args) {
 		SecondHello secondHello = new SecondHello();
 		secondHello.setAge(88);
+		System.out.format("Hello World, I am %d years old.", secondHello.getAge());
 	}
 	
+	private int getAge() {
+		return this.age;
+	}
 	private void setAge(int _age) {
 		this.age = _age;
 	}
