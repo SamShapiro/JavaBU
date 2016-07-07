531354e1193a99792df89017314aebac9ebe6f0e
Sam Shapiro <samuel.shapiro123@gmail.com>
2016-07-06 20:22:06 -0400

    modified secondHello

--- second/src/main/java/second/SecondHello.java
+++ second/src/main/java/second/SecondHello.java
@@ -3,6 +3,8 @@ package second;
 public class SecondHello {
 	private int age;
 	
+	private char ch;
+	
 	public static void main(String[] args) {
 		SecondHello secondHello = new SecondHello();
 		secondHello.setAge(88);
