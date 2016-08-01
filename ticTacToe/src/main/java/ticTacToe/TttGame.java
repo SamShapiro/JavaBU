ce94455f47f7994a7da352a151076cf18368e5c7
Sam Shapiro <samuel.shapiro123@gmail.com>
2016-08-01 13:34:28 -0400

    completed Tic Tac Toe game in Java

--- ticTacToe/src/main/java/ticTacToe/TttGame.java
+++ ticTacToe/src/main/java/ticTacToe/TttGame.java
@@ -2,87 +2,273 @@ package ticTacToe;
 
 import java.util.Scanner;
 
+/**
+ * For Java programming class, Tic Tac Toe game for two players.
+ * 
+ * @author Samuel Shapiro <samuel.shapiro123@gmail.com>
+ *
+ */
+
 public class TttGame {
-	Scanner scanner;
+	static Scanner scanner; //scanner is used to get input on placement from users
 	
-	public static final int EMPTY = 0;
-	public static final int EX = 1;
-	public static final int OH = 2;
+	private enum Fill {EMPTY, X, O, TIE} //enum Fill places either an X or an O in a cell
 	
-	public static int size;
-	public static int[][] board;
-	public static int winner;
+	private int size; //determines size of the board. defaults to 3
+	private Fill[][] board; // the board is an array of Fills (X's and O's)
+	private Fill winner; //determines winner of game
+	private String placement; //input String for places a Fill
+	private int[] pos; //int version of placement
 	
+	/**
+	 * Constructor. Defaults to a 3x3 game with no winner.
+	 */
 	public TttGame() {
-		size = 3;
-		board = new int[size][size];
+		setSize(3);
+		setBoard(getSize());
+		setWinner(Fill.EMPTY);
 	}
 	
 	public static void main(String[] args) {
-		size = 3;
-		board = new int[size][size];
-		printBoard();
+		TttGame game = new TttGame();
+		scanner = new Scanner(System.in);
+		int turnCount = 0;
+		do { //until a winner is determined, keep asking for placements
+			game.printBoard();
+			System.out.print("Player 1, enter a row/column position: ");
+			game.placement = scanner.nextLine();
+			game.pos = game.getPos(game.placement); //user input is verified in getPos
+			
+			game.getBoard()[game.pos[0]][game.pos[1]] = Fill.X;
+			game.checkWin(game.pos[0], game.pos[1]);
+			turnCount++;
+			
+			System.out.print("\n\n\n\n\n\n"); //prints a bunch of empty lines to "clear" the console
+			
+			if (turnCount == game.getSize() * game.getSize()) //if all spaces have been filled, game is a Tie
+				game.setWinner(Fill.TIE);
+			
+			if (game.getWinner() != Fill.EMPTY) //if player 1 wins, don't ask player 2
+				break;
+			
+			//repeats the above placement, but with O instead of X
+			game.printBoard();
+			System.out.print("Player 2, enter a row/column position: ");
+			game.placement = scanner.nextLine();
+			game.pos = game.getPos(game.placement);
+			game.getBoard()[game.pos[0]][game.pos[1]] = Fill.O;
+			game.checkWin(game.pos[0], game.pos[1]);
+			turnCount++;
+			
+			System.out.print("\n\n\n\n\n\n");
+			
+			if (turnCount == game.getSize() * game.getSize())
+				game.setWinner(Fill.TIE);
+			
+			
+		} while (game.getWinner() == Fill.EMPTY);
+		
+		game.printBoard();
+		if (game.getWinner() == Fill.TIE)
+			System.out.println("Game is a tie. No winner.");
+		else {
+			System.out.printf("Player %s wins!%n",
+					(game.getWinner().equals(Fill.X) ? "1" : "2")); //if winner is X, player 1 wins
+			scanner.close();										//if winner is ), player 2 wins
+
+		}
 	}
 	
-	public static void printBoard() {
-		for (int row = 0; row < size; row++) {
-			for (int col = 0; col < size; col++) {
-				printCell(board[row][col]);
-				if (col != size-1) {
+	/**
+	 * Method to convert String input to usable ints
+	 * @param place - String of user input
+	 * @return pos[] as int array version of input
+	 */
+	public int[] getPos(String place) {
+		String[] arr = place.split(" "); //splits input into String numbers
+		int count = arr.length;
+		int[] pos = new int[count];
+		for (int i = 0; i < count; i++) {
+			try{
+				pos[i] = Integer.parseInt(arr[i]);
+			}
+			//if input isn't actually a number, asks user to replace with a number
+			catch(NumberFormatException nfe){
+				System.out.printf("'%s' is not a number. Please enter a single coordinate: ", arr[i]);
+				String retry = scanner.nextLine();
+				arr[i] = retry;
+				i = -1;
+			}
+		}
+		
+		//if there aren't 2 coordinates (row and column), gets new input
+		if (pos.length != 2){ 
+			System.out.print("Incorrect number of coordinates. Try again: ");
+			String retry = scanner.nextLine();
+			pos = getPos(retry);
+		}
+		
+		//if row and column aren't in valid size range, gets new input
+		if (pos[0] < 0 || pos[0] > getSize()-1 || pos[1] < 0 || pos[1] > getSize()-1) {
+			System.out.print("That is not a valid space. Try again: ");
+			String retry = scanner.nextLine();
+			pos = getPos(retry);
+		}
+		
+		//if attempting to place in a non-empty spot, gets new input
+		if (getBoard()[pos[0]][pos[1]] != Fill.EMPTY) {
+			System.out.print("That space is already filled. Try again: ");
+			String retry = scanner.nextLine();
+			pos = getPos(retry);
+		}
+		return pos;
+	}
+	
+	/**
+	 * Prints the current game board to the console. Prints column numbers along top
+	 * and row numbers down the side.
+	 */
+	public void printBoard() {
+
+		//prints coordinates above each column
+		System.out.print("  ");
+		for (int i = 0; i < getSize(); i ++) {
+			System.out.printf(" %d  ", i);
+		}
+		System.out.println();
+		
+		for (int row = 0; row < getSize(); row++) {
+			System.out.printf("%d ", row); //prints coordinates beside each row
+			for (int col = 0; col < getSize(); col++) {
+				printCell(getBoard()[row][col]); //prints what is in each cell of the board
+				if (col != getSize()-1) {
 					System.out.print("|");
 				}
 			}
 			System.out.println();
-			if (row != size - 1) {
-				System.out.println("-----------");
+			if (row != getSize() - 1) {
+				System.out.print("  --");
+				for (int i = 0; i < getSize(); i++) {
+					System.out.print("---");
+				}
+				System.out.println();
 			}
 		}
 	}
 	
-	public static void printCell(int content) {
+	/**
+	 * Prints the contents of the cell
+	 * @param content - enum Fill of what's in the given cell
+	 */
+	public void printCell(Fill content) {
 		switch (content) {
-		case EMPTY: System.out.print("   "); break;
-		case EX: System.out.print(" X "); break;
-		case OH: System.out.print(" O "); break;
+		case X: System.out.print(" X "); break;
+		case O: System.out.print(" O "); break;
+		default: System.out.print("   ");
 		}
 	}
 	
-	public static void checkWin(int row, int col) {
+	/**
+	 * Checks to see if the most recent move has won the game
+	 * @param row - row coordinate of input
+	 * @param col - column coordinate of input
+	 */
+	public void checkWin(int row, int col) {
+		checkRow(row, col);
+		checkCol(row, col);
 		
+		//if the input is on a diagonal line, check winner for that diagonal
+		if (row == col)
+			checkDiag(row, col);
+		if (row == getSize()-1-col)
+			checkAntiDiag(row,col);
 	}
 	
-	public static void checkRow(int row) {
-		int lineSize = 0;
-		for (int box : board[row]) {
-			if (box == row) {
-				lineSize++;
+	/**
+	 * Checks if the input's row is all the same Fill
+	 */
+	private void checkRow(int row, int col) {
+		for (Fill box: getBoard()[row]) {
+			if (box != getBoard()[row][col])
+				return;
+		}
+		setWinner(getBoard()[row][col]);
+	}
+	
+	/**
+	 * Checks if the input's column is all the same Fill
+	 */
+	private void checkCol(int row, int col) {
+		for (Fill[] box: getBoard()) {
+			if (box[col] != getBoard()[row][col])
+				return;
+		}
+		setWinner(getBoard()[row][col]);
+	}
+	
+	/**
+	 * If the input was on the top-left to bottom-right diagonal, check if that diagonal
+	 * is all the same Fill
+	 */
+	private void checkDiag(int row, int col) {
+		for (int i = 0; i < getSize(); i++){
+			if (getBoard()[i][i] != getBoard()[row][col])
+				return;
+		}
+		setWinner(getBoard()[row][col]);
+	}
+	
+	/**
+	 * If the input was on the top-right to bottom-left diagonal, check if that diagonal
+	 * is all the same Fill
+	 */
+	private void checkAntiDiag(int row, int col) {
+		for (int i = 0; i < getSize(); i++) {
+			if (getBoard()[i][getSize()-1-i] != getBoard()[row][col])
+				return;
+		}
+		setWinner(getBoard()[row][col]);
+	}
+	
+
+	/**
+	 * 
+	 * getter and setter for size
+	 */
+	public int getSize() {
+		return size;
+	}
+	public void setSize(int size) {
+		this.size = size;
+	}
+
+	/**
+	 * getter and setter for the board.
+	 * setter takes a size input and makes a square board of that size
+	 * 
+	 */
+	public Fill[][] getBoard() {
+		return board;
+	}
+	public void setBoard(int size) {
+		this.board = new Fill[size][size];
+		for (Fill[] row : board) {
+			for (int i = 0; i < row.length; i++) {
+				row[i] = Fill.EMPTY; //initializes board to be filled with EMPTY enum
 			}
 		}
-		if (lineSize == size) {
-			winner = row;
-		}
 	}
-	
-	public static void checkCol(int col) {
-		int lineSize = 0;
-		for (int[] row : board) {
-			if (row[col] == col) {
-				lineSize++;
-			}
-		}
-		if (lineSize == size) {
-			winner = col;
-		}
-		
+
+	/**
+	 * getter and setter for winner
+	 * if any of the checkWin() submethods find a winner, sets it to be 
+	 * the most recent input Fill
+	 * 
+	 */
+	public Fill getWinner() {
+		return winner;
 	}
-	
-	public static void checkDiag(int row, int col) {
-		if (size % 2 == 0
-				|| (!(row == 0 || row == size-1)
-				&& !(col == 0 || col == size-1))
-				&& !(row == (size-1)/2 && col == (size-1)/2) ) {
-			return;
-		}
-		
+	public void setWinner(Fill winner) {
+		this.winner = winner;
 	}
 }
